9a87003e9d584eef662afd2ef07c6b91a36f7ad5
Inaelson Franklin Alves Ferreira <inaelsonfranklin@gmail.com>
2024-07-30 21:07:18 -0300

    added functions to randomize size, direction and timing to show or remove element.

--- game.js
+++ game.js
@@ -13,6 +13,14 @@ ajustaTamanhoPalcoJogo()
 // Cria uma posição randômica para o mosquito
 function posicaoRandomica (){
 
+    // Removendo o mosquito (caso exista)
+    var verificaMosquito = document.getElementById('mosquito')
+
+    if(verificaMosquito){
+        verificaMosquito.remove()
+    }
+
+
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -21,12 +29,13 @@ function posicaoRandomica (){
 
     console.log(posicaoX, posicaoY)
 
-    // Criar o elemento html
+    // Criar o elemento html 'mosquito'
 
     var mosquito = document.createElement('img')
 
     mosquito.src = 'imagens/mosca.png'
-    mosquito.className = 'mosquito1'
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+    mosquito.id = 'mosquito'
 
     // Define a posição
 
@@ -35,6 +44,33 @@ function posicaoRandomica (){
     mosquito.style.position = 'absolute'
 
     document.body.appendChild(mosquito)
+}
+
+//Define um número entre 0 e 2 para definir a classe do elemento.
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch(classe) {
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
+}
+
+// Lado A e B
+
+function ladoAleatorio() {
+    var classe = Math.floor(Math.random() * 2)
+
+    switch(classe) {
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+    }
 
 }
 
